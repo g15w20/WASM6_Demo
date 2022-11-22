55a5ecf9de5e92786f88f5202f7b9a101c25e858
laksh <laksh@LAPTOP-KL5AJ47R>
2022-11-22 16:41:56 +0530

    commit sample1

---
+++ src/test/java/Demo/Sample1Test.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+	
+	@Test()
+	
+	public void stest()
+	{
+		System.out.println("New Statement added here");
+	}
+
+}
